317c00da83c830e8fbbd742dbe86335adc1cc52c
Plachy Michal <plachymichal@Plachys-MacBook-Pro.local>
2018-12-31 00:06:03 +0700

    toogle menu

--- resources/views/layouts/list_group.blade.php
+++ resources/views/layouts/list_group.blade.php
@@ -2,7 +2,7 @@
 <ul class="main-list">
     <li><a href="">Home</a></li>
     <li><a href="">About</a></li>
-    <li class="active"><a href="#">Products</a>
+    <li class="has_sub"><a href="#">Products</a>
         <ul class="submenu">
         <li><a href="">Tops</a></li>
         <li><a href="">Bottoms</a></li>
@@ -11,3 +11,8 @@
     </li>
     <li><a href="">Contact us</a></li>
 </ul>
+<script>
+$(".has_sub").click(function() {
+    $(this).toggleClass( "active" );
+}) 
+</script>
